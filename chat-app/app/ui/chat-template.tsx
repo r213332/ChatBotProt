56a599e7f363091722f963588bba989e503c775f
s.Riku <shimokurosawa.riku.qs@tut.jp>
2024-11-13 22:05:36 +0900

    アイコン追加

--- chat-app/app/ui/chat-template.tsx
+++ chat-app/app/ui/chat-template.tsx
@@ -1,4 +1,5 @@
 import { Switch, Input } from "@nextui-org/react";
+import { Bot, BookOpenText } from "lucide-react";
 
 export function ChatTemplate({
   rag,
@@ -11,14 +12,24 @@ export function ChatTemplate({
 }) {
   return (
     <div className="flex w-full p-5 justify-between items-center">
-      <div className="bg-gray-400 w-[30%] h-full rounded-md p-3">
-        <h2>🤖チャット</h2>
+      <div className="bg-gray-400 w-[30%] h-full rounded-md p-3 flex flex-col">
+        <div className="flex items-center gap-2">
+          <div className="bg-green-200 text-gray-400 w-8 h-8 rounded-full flex justify-center items-center">
+            <Bot />
+          </div>
+          <h2>チャット</h2>
+        </div>
         <p>
           様々な内容をチャットできます。プログラムの質問やレポートの校正をしてみてください。ローカルで動作しているので内容は保存されません。
         </p>
       </div>
       <div className="bg-gray-400 w-[68%] h-full rounded-md p-3 flex flex-col">
-        <h2>RAG</h2>
+        <div className="flex items-center gap-2">
+          <div className="bg-green-200 text-gray-400 w-8 h-8 rounded-full flex justify-center items-center">
+            <BookOpenText />
+          </div>
+          <h2>RAG</h2>
+        </div>
         <p>
           ファイルをアップロードしてその内容についてチャットします。ファイルは保存されません。
           PDFとTXTファイルのみ対象です。
